e36a91c1a38ac0b3b6bd7d92089bf45231ed5c44
snsart <snsart@163.com>
2019-02-19 17:25:54 +0800

    add

---
+++ demo1/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class App extends React.Component {
+	render() {
+	   	return (
+	      <div>hello world</div>
+	    );
+	}
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+
