2d226c4ed75843818807e26ff10a240aedc47a61
unknown <snsart@163.com>
2019-09-10 19:53:05 +0800

    add

---
+++ r11-一个模板应用/js/source/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo';
+
+ReactDOM.render(
+	<h1>
+		<Logo/> Welcome to react!!!!
+	</h1>,
+	document.getElementById("app")
+);
