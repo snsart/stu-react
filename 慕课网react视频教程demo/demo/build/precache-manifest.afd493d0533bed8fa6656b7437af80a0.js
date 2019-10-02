4321cc303be9939a70eb1cffcf2eb5ca5947e138
snsart <snsart@163.com>
2019-10-02 14:03:02 +0800

    add

---
+++ 慕课网react视频教程demo/demo/build/precache-manifest.afd493d0533bed8fa6656b7437af80a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cf2eaffc12ae8b9dac3aa7f89127c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9653c8fae3eeef97dee9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "092d8adefa71bfb82cbf",
+    "url": "/static/js/2.69db48a1.chunk.js"
+  },
+  {
+    "revision": "9653c8fae3eeef97dee9",
+    "url": "/static/js/main.7f14a4dd.chunk.js"
+  },
+  {
+    "revision": "66ff345f11d0a39b8eac",
+    "url": "/static/js/runtime-main.83206213.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
